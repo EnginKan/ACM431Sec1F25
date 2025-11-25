ed46a47dab84bf2bfbb3a5331e76c45afd9d3bae
engin.kandiran <engin.kandiran@gmail.com>
2025-11-25 16:27:29 +0300

    Week 8: How to get data from WebService illustrated. Usage of Retrofit Lib. Components: FakeStoreAPIService.kt RetrofitAPI.kt

--- app/src/main/java/com/yeditepe/acm43project/week9/network/viewmodel/ProductViewModel.kt
+++ app/src/main/java/com/yeditepe/acm43project/week9/network/viewmodel/ProductViewModel.kt
@@ -1,19 +1,19 @@
 package com.yeditepe.acm43project.week9.network.viewmodel
 
 import androidx.lifecycle.ViewModel
-import com.yeditepe.acm43project.week8.viewmodel.Product
+import com.yeditepe.acm43project.week9.network.Product
 import com.yeditepe.acm43project.week9.network.Client
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 
 class ProductViewModel: ViewModel() {
 
-    var _products: MutableStateFlow<List<Product>> = MutableStateFlow(List<Product>())
+    var _products = MutableStateFlow<List<Product>>(listOf())
+
+    val products = _products.asStateFlow()
     init{
         getProduct()
     }
-    val products = _products.asStateFlow()
-
 
     fun getProduct(){
         _products.value = Client.service.getProducts()
